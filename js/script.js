b7b43089177310c17406a0cda4a085f7a0c4c543
Joel Armando <joelarma@gmail.com>
2015-10-29 20:39:14 +0000

    Add NYT api and getJSON() function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $img = $('#background');
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -19,7 +20,22 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+ address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    $img.attr('src', streetviewUrl);
+    $img.attr('alt', 'address image');
+
+    $.getJSON('http://api.nytimes.com/svc/search/v2/articlesearch.json?'+
+                'fq=glocations("' + cityStr + '")&'+
+                'api-key=87421a94f5a2872fee7af9e41e8049ef:6:73318691', function(data) {
+        var items = [];
+        $.each(data.response.docs, function(index, article) {
+            var item = "<li id='article-" + index + "' class='article'>" +
+            "<a href='"+ article.web_url +"'>"+ article.headline.main +"</a> <p>" +
+            article.snippet + "</p></li>";
+            items.push(item);
+        });
+
+        $nytElem.html(items.join(""));
+    });
 
     return false;
 };
